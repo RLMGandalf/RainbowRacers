38f0a63b1dad10dca4fe1394cfb91001e27d77df
Benjamin Rianda <reptarforlife@gmail.com>
2015-02-05 02:48:23 -0800

    getting it down to cars and win condition in lists

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -4,13 +4,7 @@ function BuildHotkeyModel() {
 
     self.SpecIdToGridMap = ko.observable(
         {
-            "/pa/units/land/radar/radar.json": ["factory", 13],
-            "/pa/units/land/bot_factory/bot_factory.json": ["factory", 14],
-			"/pa/units/land/energy_plant/energy_plant.json": ["factory", 12],
-			"/pa/units/land/metal_extractor/metal_extractor.json": ["factory", 11],
-
-			"/pa/units/land/assault_bot/assault_bot.json": ["bot", 11],
-            "/pa/units/land/fabrication_bot_combat/fabrication_bot_combat.json": ["bot", 12],
+			"/pa/units/land/metal_extractor/metal_extractor.json": ["factory", 11]
         }
     );
 };
