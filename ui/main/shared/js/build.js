b49379b1e24bc52b0cd0ca58445cee2f55518bda
Benjamin Rianda <reptarforlife@gmail.com>
2015-02-10 13:01:09 -0800

    name,. build js and unit list update

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -5,7 +5,8 @@ function BuildHotkeyModel() {
     self.SpecIdToGridMap = ko.observable(
         {
 			"/pa/units/land/laser_defense_single/laser_defense_single.json": ["factory", 11],
-			"/pa/units/land/land_scout/land/scout.json": ["factory", 12]
+			"/pa/units/land/land_barrier/land_barrier.json": ["factory", 12],
+			"/pa/units/land/land_scout/land_scout.json": ["factory", 13]
         }
     );
 };
